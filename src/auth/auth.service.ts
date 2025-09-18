42e156ec4e44f2cc1bd966b1fd5821daef4b2de6
Luis César <luiscesarn1@gmail.com>
2025-09-17 22:24:52 -0300

    criando rota de cadastro

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  BadRequestException,
+  Logger,
+} from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
 import { CadastroDTO, LoginDTO } from './DTOS/auth';
+import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class AuthService {
-  async cadastra(cad: CadastroDTO) {
-    console.log(cad);
-    return cad;
+  private readonly logger = new Logger(AuthService.name);
+
+  constructor(private prismaService: PrismaService) {}
+
+  async cadastra(cad: CadastroDTO): Promise<CadastroDTO> {
+    try {
+      return await this.prismaService.user.create({
+        data: cad,
+      });
+    } catch (error: unknown) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new ConflictException('Já existe uma conta com esse email');
+      }
+
+      this.logger.error('Erro ao criar um usuário', (error as Error).stack);
+      throw new BadRequestException('Erro ao criar um usuário');
+    }
   }
 
   async login(log: LoginDTO) {
-    console.log(log);
-
+    this.logger.debug(`Login tentado com: ${JSON.stringify(log)}`);
     return log;
   }
 }
